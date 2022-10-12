7164d547aeb99ed9fc39361f51981393eaa31895
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-12 14:47:40 +0300

    navbar done

---
+++ src/helper/data.js
@@ -0,0 +1,86 @@
+export const data = [
+  {
+    id: 1,
+    title: "Bursa Dünya Kültür Mirası",
+    desc: "Bursa’nın tarihi yerleri, sadece Türk tarihi için değil, insanlık tarihi açısından önemli sayıldığından UNESCO tarafından Dünya Kültür Mirası olarak koruma altına alınmaya uygun görülmüş yerler. Bu nedenle yerli yabancı her turistin öncelikleri arasında Bursa Gezilecek Yerler  denilince Osmanlı’yı bir beylikten imparatorluğa taşıyan Bursa’nın tarihi yerleri geliyor. ",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-tarihi-yerler-ulu-cami-1155x675.jpg",
+  },
+  {
+    id: 2,
+    title: "BURSA NEDEN BİR DÜNYA KÜLTÜR MİRASI?",
+    desc: " Bursa tarih boyunca Osmanlı Devleti için en önemli şehirlerden olmuş. İpek yolu üzerinde sahip olduğu hanlar sayesinde sahip olduğu ticari rol; Osmanlı’daki Vakıf Sistemi temelli sosyal yaşama ışık tutması, Bursa’nın tarihi yerleri dediğimiz külliye, camii, han ve hamamlarının gerçekten otantik olması, Bursa’dan çıkma Osmanlı mimarisini yansıtan yapılara ev sahipliği yapması ve basitçe Osmanlı’nın ikinci başkenti olması birçok nedenden birkaçı sadece.",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-tarihi-yapilar.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Bir Kültür Başkenti Olarak Bursa",
+    desc: "Bursa en eski Osmanlı dönemlerinden itibaren; önemli tarihi olaylara, efsanelere ve geleneklere ev sahipliği yapan bir şehir. Şehrin mistik imajını anlatmak için, eski Osmanlı sultanlarının türbelerini veya Hacıvat ve Karagöz’ün Orhan Gazi Külliyesi’nde çalışan işçiler oldukları efsanelerini örnek verebiliriz.",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-hacivat-karagoz.jpg",
+  },
+  {
+    id: 4,
+    title: "Bursa Tipi Mimari",
+    desc: "Bursa üslubu, Edirne’ye oradan İstanbul’a kadar taşınan çok geniş bir etki alanına sahip bir mimari üsluptür.Bursa’yı ise bu üslubun köklerinin bulunduğu yer olarak da düşünebiliriz. “Bursa stili” veya “ters t tipi” diye adlandırılan mimari plan, külliyelerin ve camilerin oluşumunda yalnızca Osmanlı’da kullanılan bir plandı.",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/emir-sultan-cami-bursa.jpg",
+  },
+  {
+    id: 5,
+    title: "Hanlar Bölgesi",
+    desc: "Osmanlı Dönemi’nde yapılan ve Bursa’nın tarihi yerleri arasında oldukça eski olan Hanlar Bölgesi’ndeki hanlardan en bilinenleri: Apolyont Han, Emir Han, Eskişehir Hanı, Kubbeli Han, Balibey Han, Geyve Han ve en önemlilerinden biri olan Koza Han.",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-kozahan-avlu.jpg",
+  },
+  {
+    id: 6,
+    title: "Ulu Cami",
+    desc: "Ulu Cami ise Hanlar Bölgesi’nin incisi, en değerli yapısı, Evliya Çelebi‘nin deyimiyle “Bursa’nın Ayasofyası!”. 1. Bayezid’in mimarı Ali Neccar’a yaptırılmış olan cami yaklaşık 600 yıllık. Bursa’da sadece tek bir tarihi yapı görme hakkınız olsa tercihiniz Ulu Cami’den yana olsun. Özellikle de Samanyolu Galaksisi’nin Kündekâri sanatı ile işlendiği minberi çok özel.",
+    image: "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-ulu-cami.jpg",
+  },
+  {
+    id: 7,
+    title: "Hüdavendigar Külliyesi (1. Murad)",
+    desc: "1. Murad’ın yaptırdığı bu külliye, halk arasında 1. Murad’a Hüdavendigar da denmesi nedeniyle bu ismi almış. Külliye Bursa’nın tepelerinden birinde yer alıyor ve içinde cami, medrese, türbe, çeşme ve hamam var. Külliyede 1. Murad (Hüdavendigar) Camii var ve bu cami de 1360’lı yıllarda yaptırılmış. Osmanlı’nın meşhur “ters T planlı” mimarisiyle yapılmış. ",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-hudavendigar-kulliyesi.jpg",
+  },
+  {
+    id: 8,
+    title: "Yıldırım Külliyesi (1. Bayezid)",
+    desc: "1390’lerde yapılan Yıldırım Külliyesi de Yıldırım Bayezid tarafından yaptırılmış. İçinde medrese, cami darüşşifa, hamam, imaret ve türbe var. Bursa’da Yıldırım tepesinde bulunan camii yine “Ters T” planlıdır. ",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/yildirim-kulliyesi.jpg",
+  },
+  {
+    id: 9,
+    title: "Yeşil Külliye (1. Mehmed)",
+    desc: "Yeşil Külliye’yi 1. Mehmed 1400’lü yıllarda yaptırmış. İçinde medrese, cami, türbe ve hamam var. Yeşil Külliyesi Osmanlı’nın tam düşmek üzereyken kalkıp güçlendiği bir dönemde yapıldığı için bir nevi geri dönüş olarak görülmüş ve oldukça göz alıcı yapılmış. ",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-yesil-cami.jpg",
+  },
+  {
+    id: 10,
+    title: "Orhangazi Külliyesi",
+    desc: "1300’lü yıllarda Orhan Bey’in yaptırdığı; içinde cami, külliye, medrese, mektep, hamam ve Emir Han adında bir han bulunan külliye. Külliyedeki yapılardan günümüzde ayakta kalmış üç yapı var; Orhan Cami, Emir Han ve Orhan Hamamı.",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-orhangazi-kulliyesi.jpg",
+  },
+  {
+    id: 11,
+    title: "Muradiye Külliyesi (2. Murad)",
+    desc: "Bu külliye Osmanlı İmparatorluğu padişahları tarafından yaptırılan külliyelerden sonuncusu. 1420 yıllarında yapılmış ve burası da bulunduğu bölgeye ismini vermiş. İçinde medrese, cami, hamam, darüşşifa ve türbe var. Muradiye Camii’nin süslemeleri çok ihtişamlı, bir sürü motifler ve kullanılan sanat türleri Osmanlı’nın sanat bakış açısı hakkında fikir sahibi olmamızı sağlıyor. ",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/bursa-muradiye-kulliyesi.jpg",
+  },
+  {
+    id: 12,
+    title: "Cumalıkızık Köyü",
+    desc: "Cumalıkızık’ın listeye alınması UNESCO tarafından şöyle açıklanmış: Bursa’daki sistemi gösterebilecek ve ona yardım sağlayabilen tek kırsal köy.Çünkü bu sistemde merkezdeki vakıf binaları yani külliyeler kırsal kesimle de bağlantılıymış.Cumalıkızık köyü merkezdeki Orhan Gazi Külliyesi’ne kalıcı olarak bağlı olup ona gelir sağladığı için “vakıf köyü” olarak geçmiştir.",
+    image:
+      "https://www.bizevdeyokuz.com/wp-content/uploads/Cumalikizik-renkli-evler.jpg",
+  },
+];
